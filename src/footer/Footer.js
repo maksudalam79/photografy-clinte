b1e92cae3c590243d4ea004cb6b93259db755bc1
maksudalam <maksud.cdz@gmail.com>
2022-11-08 11:13:29 +0600

    footer complite

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../other/camera-logo-cliparts-30.png'
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Footer</h2>
-        </div>
+        <footer className="footer p-10 bg-base-200 text-base-content">
+  <div>
+   <img className='w-10' src={img} alt="" />
+    <p className='font-bold'>photography</p>
+    <p><br/>When words become unclear,<br/>I shall focus with photographs.</p>
+  </div> 
+  <div>
+    <span className="footer-title">Services</span> 
+    <Link className="link link-hover">Branding</Link> 
+    <Link className="link link-hover">Design</Link> 
+    <Link className="link link-hover">Marketing</Link> 
+    <Link className="link link-hover">Advertisement</Link>
+  </div> 
+  <div>
+    <span className="footer-title">Company</span> 
+    <Link className="link link-hover">About us</Link> 
+    <Link className="link link-hover">Contact</Link> 
+    <Link className="link link-hover">Jobs</Link> 
+    <Link className="link link-hover">Press kit</Link>
+  </div> 
+  <div>
+    <span className="footer-title">Legal</span> 
+    <Link className="link link-hover">Terms of use</Link> 
+    <Link className="link link-hover">Privacy policy</Link> 
+    <Link className="link link-hover">Cookie policy</Link>
+  </div>
+</footer>
     );
 };
 
